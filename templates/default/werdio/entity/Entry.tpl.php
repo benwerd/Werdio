d2483411713ecc25c7e03fd4be215ec2271c27ca
Ben Werdmuller <ben@benwerd.com>
2022-10-11 11:53:23 -0700

    Improved style

--- templates/default/werdio/entity/Entry.tpl.php
+++ templates/default/werdio/entity/Entry.tpl.php
@@ -11,16 +11,16 @@ if (\Idno\Core\Idno::site()->currentPage()->isPermalink()) {
             target="popupwindow"
             onsubmit="window.open('https://newsletter.werd.io', 'popupwindow')"
             class="embeddable-buttondown-form"
-            style="font-size: 1.2em; line-height: 1.4em"
+            style="font-size: 1.2em; line-height: 1.8em"
             >
                 <div style="margin: auto; text-decoration: center">
-                    <input type="email" name="email" id="bd-email" placeholder="To subscribe, enter your email address" style="color: rgb(0, 0, 0); border-color: rgb(227, 227, 227); border-radius: 4px; font-weight: 400; padding: 10px; margin: 0; width: 70%" />
-                    <input type="button" class="btn btn-primary" value="Subscribe" style="padding: 10px; margin: 0; margin-top: -2px; font-size: 1em; width: 20%" />
+                    <input type="email" name="email" id="bd-email" placeholder="To subscribe, enter your email address" style="color: rgb(0, 0, 0); border-color: rgb(227, 227, 227); border-radius: 4px; font-weight: 400; padding: 10px; margin: 0; width: 70%; min-width: 300px" />
+                    <input type="button" class="btn btn-primary" value="Subscribe" style="padding: 10px; margin: 0; margin-top: -2px; font-size: 1em; width: 20%; min-width: 100px" />
                 </div>
             </form>
-            <p style="margin-top: 21px; font-size: 1.2em">
-		        <a href="https://twitter.com/benwerd">Follow me on Twitter</a> <span style="padding-left: 15px; padding-right: 15px">|</span> 
-                <a href="mailto:ben@werd.io">Email me</a> at ben@werd.io
+            <p style="margin-top: 21px;">
+		        <div style="display: inline-block"><a href="https://twitter.com/benwerd">Follow me on Twitter</a> <span style="padding-left: 15px; padding-right: 15px">|</span></div>
+                <div style="display: inline-block"><a href="mailto:ben@werd.io">Email me</a> at ben@werd.io</div>
               </p>
           </div>
       </div>
