a532b01424b0788d74727fa9d0aaa64973d48d64
Ben Werdmuller <ben@benwerd.com>
2022-11-30 01:30:18 +0000

    Adding donation box for The 19th

--- templates/default/werdio/entity/Entry.tpl.php
+++ templates/default/werdio/entity/Entry.tpl.php
@@ -3,6 +3,17 @@
 if (\Idno\Core\Idno::site()->currentPage()->isPermalink()) {
 
     ?>
+      <div style="padding:25px; background-color: rgb(255, 229, 0); font-size: 1.3em">
+        <h2>Can you help?</h2>
+	<p>
+	  My colleagues at <a href="https://19thnews.org">The 19th</a> are looking for 350 people to join as members. It costs as little
+	  as $19 and you'll support a diverse, women-led newsroom doing important reporting at the intersection of gender, politics, and
+	  policy in the US.
+	</p>
+	<p>
+	  If you have the means, <a href="https://the19th.donorsupport.co/-/NVXBQRUH?2">please donate to The 19th today</a>.
+	</p>
+      </div>
       <div class="row annotation-add">
           <div class="col-md-12 idno-comment-container" id="comment-form" >
             <div style="margin: auto; margin-bottom: 25px;">
