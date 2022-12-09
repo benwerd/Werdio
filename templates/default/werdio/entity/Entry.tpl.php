b9ae643cac3a87ff94208228bc2b1c5d74af8167
Ben Werdmuller <ben@benwerd.com>
2022-12-09 18:46:42 +0000

    Better 19th messaging

--- templates/default/werdio/entity/Entry.tpl.php
+++ templates/default/werdio/entity/Entry.tpl.php
@@ -6,10 +6,14 @@ if (\Idno\Core\Idno::site()->currentPage()->isPermalink()) {
       <div style="padding:25px; background-color: rgb(255, 229, 0); font-size: 1.3em">
         <h2>Can you help?</h2>
 	<p>
-	  My colleagues at <a href="https://19thnews.org">The 19th</a> are looking for 350 people to join as members. It costs as little
-	  as $19 and you'll support a diverse, women-led newsroom doing important reporting at the intersection of gender, politics, and
-	  policy in the US.
+	  <a href="https://19thnews.org/">The 19th</a> is a diverse, women-led newsroom that reports at the intersection of gender, politics,
+	  and policy in the US. It was one of the only newsrooms to call the election correctly because it listened to women and particularly
+	  women of color. It provides important representation for vulnerable communities who have been underserved by the media. And it needs
+	  your help.
 	</p>
+        <p>
+          It's a non-profit that is making a big impact with few resources. As little as $5 could make a huge difference in this reporting.
+        </p>
 	<p>
 	  If you have the means, <strong><a href="https://the19th.donorsupport.co/-/NVXBQRUH?2" style="font-weight: 800">please donate to The 19th today</a></strong>.
 	</p>
