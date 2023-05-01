dfdc27a5bfb0da06c75ada5fe63849ec8a0af475
Ben Werdmuller <ben@benwerd.com>
2023-05-01 21:36:24 +0000

    Fixing Substack problem

--- templates/default/werdio/entity/Entry.tpl.php
+++ templates/default/werdio/entity/Entry.tpl.php
@@ -1,25 +1,9 @@
 <?php
 
-if (\Idno\Core\Idno::site()->currentPage()->isPermalink()) {
+$entity = \Idno\Core\Idno::site()->currentPage()->getEntity();
 
-/* CTA
-    ?>
-      <div style="padding:25px; background-color: rgb(255, 229, 0); font-size: 1.3em">
-        <h2>Can you help?</h2>
-	<p>
-	  <a href="https://19thnews.org/">The 19th</a> is a diverse, women-led newsroom that reports at the intersection of gender, politics,
-	  and policy in the US. It was one of the only newsrooms to call the election correctly because it listened to women and particularly
-	  women of color. It provides important representation for vulnerable communities who have been underserved by the media. And it needs
-	  your help.
-	</p>
-        <p>
-          It's a non-profit that is making a big impact with few resources. As little as $5 could make a huge difference in this reporting.
-        </p>
-	<p>
-	  If you have the means, <strong><a href="https://the19th.donorsupport.co/-/NVXBQRUH?2" style="font-weight: 800">please donate to The 19th today</a></strong>.
-	</p>
-      </div>
-*/
+if (\Idno\Core\Idno::site()->currentPage()->isPermalink() && !empty($vars['object']) &&  $entity->getID() === $vars['object']->getID()) {
+
 ?>
       <div class="row annotation-add">
           <div class="col-md-12 idno-comment-container" id="comment-form" >
